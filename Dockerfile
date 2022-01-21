d40c3e243479eda9595a03b06505d8b77463a6f3
pre <preimmortal@gmail.com>
2022-01-21 00:06:36 -0800

    Add ability to change user

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM alpine:3.15
 RUN apk add --no-cache --virtual .run-deps bash rsync sqlite curl  && rm -rf /var/cache/apk/*
+
+ENV USER=backup
+ENV USERGROUP=backup
+ENV PUID=1000
+ENV PGID=1000
+
+COPY bootstrap.sh /
 COPY entrypoint.sh /
-ENTRYPOINT [ "/entrypoint.sh" ]
+
+ENTRYPOINT [ "/bootstrap.sh" ]
---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#########################################################################
+# Setup Environment
+#########################################################################
+echo "Setting up environment"
+
+if [ "${PGID}" == "0" ]; then
+  export USERGROUP=root
+else
+  addgroup -g ${PGID} ${USERGROUP}
+fi
+
+if [ "${PUID}" == "0" ]; then
+  export USER=root
+else
+  adduser \
+    --disabled-password \
+    --gecos "" \
+    --ingroup "${USERGROUP}" \
+    --uid "${PUID}" \
+    "${USER}"
+fi
+
+su - ${USER} -c /entrypoint.sh
